03bf29d31eef42bd0d3e7511543c48989e0f57c1
Victor Souza Lima <victorsouzalima2064@gmail.com>
2024-09-24 21:05:36 -0300

    *

--- Testes/Questao03/AlternativaOtimizada/Summation.c
+++ Testes/Questao03/AlternativaOtimizada/Summation.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int summation(int index){
-    return (index)*(index+1)/2;
+int summation(int limiteSuperior){
+    return (limiteSuperior)*(limiteSuperior+1)/2;
 }
 
 int main(){
-    int limiteSuperior = 12;
-    printf("%d", summation(limiteSuperior-1));
+    int index = 12;
+    printf("%d", summation(index-1));
     return 0;
 }
 
 /*
 EXPLICAÇÃO:
     Dado que a soma proposta pelo exercicio pode ser representada matematicamente pelo somatorio abaixo:
-            12
-            Σ(k)
+            11
+            Σ(k)          //12 não esta incluso pois o teto é até < 12
             k=1
     
     E este somatorio segue o formato proposto por Gauss:
@@ -26,7 +26,7 @@ EXPLICAÇÃO:
 utilizar o somatorio de Gauss e reduzir a complexidade do problema de um grau linear O(n) para um grau constante O(1), 
 gerando uma eficiencia maior na execuçaõ do programa
 
-            12
-            Σ(k) = 12(12+1)/2 = 66
+            11
+            Σ(k) = 11(11+1)/2 = 66
             k=1
 */
